ed52babbb7ca558f6f7721a59e27de2f50debeb5
basem mohamed <basemmohamed18@gmail.com>
2021-10-22 23:21:25 +0200

    edits

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -198,14 +198,6 @@ $(document).ready(function () {
     fixedContentPos: false
   });
 
-  var href = window.location.href;
-  var splitit = (href.split('#'))[1]; //split url to get sec1 name
-  if (splitit !== "" || splitit !== "undefined") {
-    $('html, body').animate({
-      scrollTop: $('#' + splitit).offset().top
-    }, 2000);
-  }
-
   $(window).on('scroll', function (event) {
     var scrollValue = $(window).scrollTop();
     if (scrollValue > 120) {
